8a0c33de7b2b5a5d0dc852265f80ae217a4222b8
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-18 21:27:43 +0530

    checking Emp is present or Absent

---
+++ EmpWagePresentAbsent/EmployeePresentAbsent/EmpPresentAbsent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeWageComputation
+{
+    public class EmpPresentAbsent
+    {
+
+     public static void Main(string[] args)
+        {
+            int isPresent = 1;
+
+            Random employee = new Random();
+            int randomInput = employee.Next(0, 2);
+
+            if(randomInput == isPresent)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+            Console.ReadLine();
+            }
+    }
+}
