e508995295e685ccca8d08940fc5046dc91e6357
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-19 15:46:33 +0530

    [Roopa]printing Welcome to EmpWageComputation Program

---
+++ EmpWageComputationProgram/EmpWageComputationProgram/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageComputationProgram
+{
+  public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Emp Wage Computation");
+            Console.ReadLine();
+        }
+    }
+}
