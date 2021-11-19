31077d176b0694c895eb7c7a130a03e2a82edaf5
Roopa Jamadar <roopajamadar14@gmail.com>
2021-11-19 10:57:37 +0530

    Swich case used

---
+++ SwitchCaseStatement/SwitchCaseStatement/SwitchStatement.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchCaseStatement
+{
+
+    public class SwitchStatement
+    {
+       public const int IS_FULL_TIME = 1;
+       public const int IS_PART_TIME = 2;
+       public const int ratePerHrs = 20;
+        public static void Main(string[] args)
+        {
+            
+            int empWage = 0;
+            int empHrs = 0;
+            string typeOfEmp = "";
+
+            Random employee = new Random();
+            int randomInput = employee.Next(0, 3);
+
+            switch (randomInput)
+            {
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+                   
+            empWage = ratePerHrs * empHrs;
+            Console.WriteLine(typeOfEmp+"  Wage per day is :" + empWage);
+            Console.ReadLine();
+        }
+    }
+}
+
+    
